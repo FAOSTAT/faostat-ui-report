1aa9ef9f815cf47328744ba07b0bc99998f83da2
simonemurzilli <simone.murzilli@fao.org>
2016-01-26 11:28:21 +0100

    fixed report call

--- src/js/start.js
+++ src/js/start.js
@@ -26,6 +26,23 @@ define([
         },
         defaultOptions = {
 
+            request: {
+                List1Codes: null,
+                List2Codes: null,
+                List3Codes: null,
+                List4Codes: null,
+                List5Codes: null,
+                List6Codes: null,
+                List7Codes: null,
+                List1AltCodes: null,
+                List2AltCodes: null,
+                List3AltCodes: null,
+                List4AltCodes: null,
+                List5AltCodes: null,
+                List6AltCodes: null,
+                List7AltCodes: null
+            }
+
         };
 
         function Report() {
@@ -40,6 +57,7 @@ define([
             this.initVariables();
             this.initComponents();
             this.configurePage();
+            this.bindEventListeners();
         };
 
         Report.prototype.initVariables = function () {
@@ -69,6 +87,8 @@ define([
                 lang: Common.getLocale(),
                 placeholder_id: s.SELECTORS,
                 domain: this.o.code,
+                report_code: this.o.code,
+                multiple: false,
                 callback: {
                     onSelectionChange: function () {
                         self.$REPORT_TABLE.empty();
@@ -80,16 +100,48 @@ define([
 
         Report.prototype.configurePage  = function () {
 
+        };
+
+        Report.prototype.table  = function (type) {
+
             this.reportTable.init({
                 container: this.$REPORT_TABLE,
-                request: {
-                    domain_code: this.o.code,
-                    List1Codes: [9],
-                    List2Codes: [2011]
-                }
+                request: this.getRequestObject()
             });
 
-            this.reportTable.render();
+            log.info(this.reportTable);
+
+            if ( type === 'export') {
+                this.reportTable.export();
+            }
+
+            if ( type === 'preview') {
+                this.reportTable.render();
+            }
+
+        };
+
+        Report.prototype.getRequestObject = function () {
+
+            var userSelection = this.download_selectors_manager.get_user_selection(),
+                obj = {
+                    List1Codes: userSelection.list1Codes || null,
+                    List2Codes: userSelection.list2Codes || null,
+                    List3Codes: userSelection.list3Codes || null,
+                    List4Codes: userSelection.list4Codes || null,
+                    List5Codes: userSelection.list5Codes || null,
+                    List6Codes: userSelection.list6Codes || null,
+                    List7Codes: userSelection.list7Codes || null,
+                    List1AltCodes: userSelection.list1AltCodes || null,
+                    List2AltCodes: userSelection.list2AltCodes || null,
+                    List3AltCodes: userSelection.list3AltCodes || null,
+                    List4AltCodes: userSelection.list4AltCodes || null,
+                    List5AltCodes: userSelection.list5AltCodes || null,
+                    List6AltCodes: userSelection.list6AltCodes || null,
+                    List7AltCodes: userSelection.list7AltCodes || null
+                };
+
+            return $.extend(true, {}, {domain_code: this.o.code}, obj);
         };
 
         Report.prototype.isNotRendered = function () {
@@ -98,16 +150,27 @@ define([
 
         Report.prototype.bindEventListeners = function () {
 
+            var self = this;
+
+            this.$PREVIEW_BUTTON.on('click', function() {
+                self.table('preview');
+            });
+            this.$EXPORT_BUTTON.on('click', function() {
+                self.table('export');
+            });
+
         };
 
         Report.prototype.unbindEventListeners = function () {
-
+            this.$PREVIEW_BUTTON.off();
+            this.$EXPORT_BUTTON.off();
         };
-
 
         Report.prototype.destroy = function () {
 
             this.unbindEventListeners();
+
+            this.$CONTAINER.empty();
 
         };
 
